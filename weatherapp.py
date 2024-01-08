43d00ef4ca798fd5ca35c9fc4c64095ff5132fcc
NickPrivate <dragonwolf478@gmail.com>
2024-01-08 11:06:52 -0800

    Command line weather app using OpenWeatherMap API

---
+++ weatherapp.py
@@ -0,0 +1,28 @@
+#Personal Project: Command Line Calculator using OpenWeatherMap API
+
+import requests
+from config import API_KEY, BASE_URL
+
+#User Input
+state = input("Enter the name of the State: ")
+city = input("Enter the name of the City: ")
+
+
+params =  {
+    'q' : f'{city},{state}',
+    'appid' : API_KEY,
+    'units': 'imperial'
+}
+
+response = requests.get(BASE_URL,params)
+
+
+
+if response.status_code == 200:
+    weather_data = response.json()
+    print(f"Weather in {weather_data['name']}, {weather_data['sys']['country']}:")
+    print(f"Temperature: {weather_data['main']['temp']}°F")
+    print(f"Description: {weather_data['weather'][0]['description']}")
+else:
+    print("Error fetching weather data.")
+
